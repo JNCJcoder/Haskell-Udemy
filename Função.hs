e3f004b977b897fd66a01e3c65ad5f9c6cdbb456
JNCJcoder <jorgenettocjunior@gmail.com>
2022-09-05 09:40:55 -0300

    Função - Função Recursiva Com Tipo

--- Função.hs
+++ Função.hs
@@ -8,3 +8,12 @@
 
 soma 1 = 1
 soma numero = soma(numero - 1) + numero
+
+-- Função Recursiva Com Tipo
+-- 
+-- Recebe um valor Inteiro e retorna um valor Inteiro.
+-- Caso o numero seja igual a 0, então fatorial(numero) = 1
+
+fatorial :: Int -> Int
+fatorial 0 = 1
+fatorial numero = fatorial(numero - 1) * numero
