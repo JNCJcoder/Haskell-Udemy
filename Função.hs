695ec38d4011f495f884598e9fa7c8cd00f08b5b
JNCJcoder <jorgenettocjunior@gmail.com>
2022-09-06 11:51:39 -0300

    Função - Função Recursiva de Múltiplos Retornos

--- Função.hs
+++ Função.hs
@@ -17,3 +17,13 @@ soma numero = soma(numero - 1) + numero
 fatorial :: Int -> Int
 fatorial 0 = 1
 fatorial numero = fatorial(numero - 1) * numero
+
+-- Função Recursiva de Multiplos Retornos
+-- 
+-- Caso o numero seja igual a 0, então fibonacci(numero) = 0
+-- Caso o numero seja igual a 1, então fibonacci(numero) = 1
+-- Caso o numero seja maior que 1, então fibonacci(numero) = fibonacci(numero - 1) + fibonacci(numero - 2)
+
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci numero = fibonacci(numero - 1) + fibonacci(numero - 2)
