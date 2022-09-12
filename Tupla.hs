4c9cb57d90ef1f547ec5fe8869d4354a6312fcf1
JNCJcoder <jorgenettocjunior@gmail.com>
2022-09-12 09:42:18 -0300

    Tupla - Extraindo informações da Tupla

--- Tupla.hs
+++ Tupla.hs
@@ -6,3 +6,13 @@
 
 somaTupla :: (Int, Int) -> (Int, Int) -> (Int, Int)
 somaTupla (a, b) (c, d) = (a + c, b + d)
+
+-- Extraindo informações da Tupla
+-- 
+-- SelecionarX retorna o primeiro valor da Tupla
+-- SelecionarY retorna o segundo valor da Tupla
+-- SelecionarZ retorna o terceiro valor da Tupla
+
+selecionarX (x, _, _) = x
+selecionarY (_, y, _) = y
+selecionarZ (_, _, z) = z
