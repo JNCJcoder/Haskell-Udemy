63e0c076d8ac8a061122ba2df54bff030f25f4ba
JNCJcoder <jorgenettocjunior@gmail.com>
2022-09-19 13:47:42 -0300

    Lista - Comparar Listas

--- Lista.hs
+++ Lista.hs
@@ -7,3 +7,15 @@
 
 listaTamanho [] = 0
 listaTamanho (x:xs) = 1 + listaTamanho xs
+
+-- Comparar Listas
+-- 
+-- Compara duas listas e retorna true caso sejam iguais.
+-- Caso as duas listas sejam diferentes retorna false
+
+comparaListas :: [Int] -> [Int] -> Bool
+comparaListas [] [] = True
+comparaListas [] _ = False
+comparaListas _ [] = False
+comparaListas (a:b) (c:d) | (a == c) = comparaListas b d
+                          | otherwise = False
