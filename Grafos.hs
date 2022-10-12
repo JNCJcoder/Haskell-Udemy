0c6a11dcc27a38c8dbc0c61ba865f1884eaca921
JNCJcoder <jorgenettocjunior@gmail.com>
2022-10-12 10:23:10 -0300

    Grafos - Grafos Adjacentes

--- Grafos.hs
+++ Grafos.hs
@@ -15,3 +15,14 @@ grafo = [
             (4, 8),
             (5, 9)
         ]
+
+-- Grafos Adjacentes
+-- 
+-- Retorna os grafos adjacentes de acordo com a vertice passada.
+
+grafosAdjacentes :: Grafo -> Vertice -> [Vertice]
+grafosAdjacentes [] _ = []
+grafosAdjacentes ((a, b):c) v
+                        | (a == v) = b:(grafosAdjacentes c v)
+                        | (b == v) = a:(grafosAdjacentes c v)
+                        | otherwise = grafosAdjacentes c v
