33f71d0e5f5c7e3b1220242061e1017809bbfc44
JNCJcoder <jorgenettocjunior@gmail.com>
2022-10-16 13:46:03 -0300

    Vetor - getArray

--- Vetor.hs
+++ Vetor.hs
@@ -13,3 +13,12 @@ getArray = array (1, 4) [(1, 'A'), (2, 'B'), (3, 'C'), (4, 'D')]
 -- Retornando Todos os Elementos
 
 elems getArray
+
+-- getArray
+-- 
+-- retorna valor da posição X do vetor
+
+getArray ! 1 -- Retorna A
+getArray ! 2 -- Retorna B
+getArray ! 3 -- Retorna C
+getArray ! 4 -- Retorna D
